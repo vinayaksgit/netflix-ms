a370dc916f4ffcbba59192d0d3b591fdcfa176ce
vinayaksgit <svinayak.30@gmail.com>
2025-01-15 06:54:27 +0530

    NetflixCode

---
+++ src/main/java/com/example/netflix_ms/NetflixMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.netflix_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixMsApplication.class, args);
+	}
+
+}
